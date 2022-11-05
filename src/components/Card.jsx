701faa39dab020959c2e32bb4aea8a5b0a938742
Irlan Dos Santos Reis <xuiexpress@gmail.com>
2022-11-04 23:51:55 -0400

    Dev: Requisito 3

---
+++ src/components/Card.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Card extends Component {
+  render() {
+    const { cardName, cardDescription } = this.props;
+    const { cardAttr1, cardAttr2, cardAttr3 } = this.props;
+    const { cardImage, cardRare, cardTrunfo } = this.props;
+    return (
+      <div>
+        <h1>Card</h1>
+        <p data-testid="name-card">{ cardName }</p>
+
+        <img
+          src={ cardImage }
+          alt={ cardName }
+          data-testid="image-card"
+        />
+
+        <span data-testid="description-card">{ cardDescription }</span>
+
+        <section data-testid="attr1-card">{ cardAttr1 }</section>
+        <section data-testid="attr2-card">{ cardAttr2 }</section>
+        <section data-testid="attr3-card">{ cardAttr3 }</section>
+
+        <section data-testid="rare-card">{ cardRare }</section>
+
+        {
+          cardTrunfo === true
+            ? <section data-testid="trunfo-card">Super Trunfo</section> : null
+        }
+      </div>
+    );
+  }
+}
+
+Card.defaultProps = {
+  cardName: 'name-card',
+  cardDescription: 'description-card',
+  cardAttr1: 0,
+  cardAttr2: 0,
+  cardAttr3: 0,
+  cardImage: '',
+  cardRare: '',
+  cardTrunfo: false,
+};
+
+Card.propTypes = {
+  cardName: PropTypes.string,
+  cardDescription: PropTypes.string,
+  cardAttr1: PropTypes.number,
+  cardAttr2: PropTypes.number,
+  cardAttr3: PropTypes.number,
+  cardImage: PropTypes.string,
+  cardRare: PropTypes.string,
+  cardTrunfo: PropTypes.bool,
+};
+
+export default Card;
